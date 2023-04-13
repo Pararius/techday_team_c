514f4192fac0762938f62a86de8235d64d0b304d
M. Barendregt <barendregt@fastmail.com>
2023-04-13 11:22:55 +0200

    add api key

--- src/description-to-json/main.py
+++ src/description-to-json/main.py
@@ -1,8 +1,23 @@
 import openai
+import json
+from treehouse.utils import SecretManager
 
-def get_features_from_description(description:str) -> dict:
+GOOGLE_PROJECT_ID = "techday-team-c"
+
+def get_features_from_description(description:str, prompt:str) -> dict:
+
+    # Get API key from Secret Manager
+    sm = SecretManager(GOOGLE_PROJECT_ID)
+    gpt_api_key = sm.get("gpt-api-key")
+    openai.api_key = gpt_api_key
+
     # Call openai to get stuff from description
-    result = {}
+    completion = openai.Completion.create(model="gpt-3.5-turbo", prompt=prompt)
+
+    # print the completion
+    print(completion.choices[0].text)
+
+    result = json.loads(completion.choices[0].text)
 
     return result
 
@@ -13,6 +28,18 @@ def handler(request):
     if "description" not in request_json:
         return "No description in request", 400
 
-    print(request_json["description"])
+    prompt = "Extract information for a listing from this description: "
+    if "prompt" not in request_json:
+        print("No prompt provided, using default")  
+    else:
+        prompt = request_json["prompt"]             
 
-    return "Done", 200
+    print(f"Starting with description: {request_json["description"][0:150]}")
+
+    listing_data = get_features_from_description(
+        description=request_json["description"], 
+        prompt=prompt)
+
+    print(listing_data)
+
+    return json.dumps(listing_data), 200
---
+++ terraform/output.tf
@@ -0,0 +1,3 @@
+output "description_to_json_url" {
+  value = google_cloudfunctions_function.descr-to-json-function.https_trigger_url
+}
