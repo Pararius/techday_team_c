414f04c2d4f43d09f89abfdd36fd2dc2f18442cc
M. Barendregt <barendregt@fastmail.com>
2023-04-13 13:30:36 +0200

    add public access

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,8 +1,16 @@
 resource "google_storage_bucket" "demo-bucket" {
-    name = "techday-teamc-demo"
-    location = var.google_cloud_default_region
+  name     = "techday-teamc-demo"
+  location = var.google_cloud_default_region
 
-    website {
-      main_page_suffix = "index.html"
-    }
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+  }
+}
+
+resource "google_storage_default_object_access_control" "public_rule" {
+  bucket = google_storage_bucket.demo-bucket.name
+  role   = "READER"
+  entity = "allUsers"
 }
